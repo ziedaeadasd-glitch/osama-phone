2161511d284a1091143007dba4f3f63fadec441f
Zaid Iyad <zaid@example.com>
2026-09-14 16:40:45 +0300

    Optimize GitHub Actions Android APK build and explicit compileSdk 34

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.osama_phone"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 34
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
